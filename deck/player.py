d3fd794e7c4a0fc60efee942857e6b8770b08bc7
Mark Norman Francis <norm@vanster.francis-palmer.net>
2022-05-28 08:03:13 +0100

    Show a progress bar while playing

--- deck/player.py
+++ deck/player.py
@@ -58,8 +58,44 @@ class Player:
             self.player.set_state(Gst.State.PLAYING)
             while self.playing:
                 time.sleep(1)
+                self.output_player_state()
         else:
             print('** no file "%s"' % track)
+
+    def output_player_state(self):
+        # FIXME a terminal wider than 80 chars
+        progress_bar_width = 60
+
+        # FIXME a track over an hour long
+        duration = self.player.query_duration(Gst.Format.TIME)[1]
+        position = self.player.query_position(Gst.Format.TIME)[1]
+        if duration < 0 or position < 0:
+            # unintialised state, don't report
+            return
+
+        progress = int((position / duration) * progress_bar_width)
+        progress_bar = ('_' * (progress-1)) + '|'
+
+        # 12345678901234567890123456789012345678901234567890123456789012345678901234567890
+        #   ▶  00:07 [__________|________________________________________________] 00:31
+        print(
+            "  ▶  %s [%s] %s" % (
+                self.minutes_seconds(position),
+                progress_bar.ljust(progress_bar_width, '_'),
+                self.minutes_seconds(duration),
+            ),
+            end='\r',
+            flush=True,
+        )
+
+    def minutes_seconds(self, t):
+        s,ns = divmod(t, 1000000000)
+        m,s = divmod(s, 60)
+        if m < 60:
+            return "%02i:%02i" %(m,s)
+        else:
+            h,m = divmod(m, 60)
+            return "%i:%02i:%02i" %(h,m,s)
 
 
 @click.command()
